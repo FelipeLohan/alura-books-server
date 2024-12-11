fca6f99894864a97d3d8d2f8a4e218f52e21926c
Felipe Lohan <felipelohan03052006@gmail.com>
2024-12-11 10:13:14 -0300

    teste.js deleted

--- teste.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const booksData = JSON.parse(fs.readFileSync("./books.json"));
-
-const newData = { id: "3", nome: "Livro legal 3"}
-
-fs.writeFileSync("./books.json", JSON.stringify([...booksData, newData]));
-
-console.log(booksData);
