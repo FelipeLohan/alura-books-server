f71994ec593d7fdf8c9444b6ce452292cc108988
Felipe Lohan <felipelohan03052006@gmail.com>
2024-12-14 09:13:40 -0300

    cors added, all alowed to acess the services

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const bookRouter = require('./routes/book.js');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors({origin: "*"}))
 
 app.use('/books', bookRouter);
 const port = 8000;
